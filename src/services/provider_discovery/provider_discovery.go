4d95f5cb2e2283c1d8a91bbe45f36761c605f73b
Eric Huang <eric.haoran.huang@gmail.com>
2021-08-14 07:13:42 -0400

    Initial discovery service

--- src/services/provider_discovery/provider_discovery.go
+++ src/services/provider_discovery/provider_discovery.go
@@ -2,8 +2,8 @@ package provider_discovery
 
 import (
 	"fmt"
+	"io"
 	"net/http"
-	"time"
 )
 
 type Server struct {
@@ -18,11 +18,45 @@ func NewServer(hostPort string) *Server{
 
 func (s *Server) Run() error {
 	muxServer := http.NewServeMux()
-	muxServer.Handle("/", http.HandlerFunc(s.greet))
+	muxServer.Handle("/", http.HandlerFunc(s.discovery))
 	return http.ListenAndServe(s.hostPort, muxServer)
 }
 
-func (s *Server) greet(w http.ResponseWriter, r *http.Request) {
-	str := fmt.Sprintf("Hello World! %s", time.Now())
-	w.Write([]byte(str))
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func (s *Server) discovery(w http.ResponseWriter, r *http.Request) {
+
+	sendRequestParseBody := func(provider string) error {
+		resp, err := http.Get(provider)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return err
+		}
+		defer resp.Body.Close()
+
+		copyHeader(w.Header(), resp.Header)
+		// write the response code
+		w.WriteHeader(resp.StatusCode)
+
+		// copies over body
+		io.Copy(w, resp.Body)
+		return nil
+	}
+
+	provider := r.FormValue("provider")
+	switch provider {
+	case "GITHUB":
+		username := r.FormValue("username")
+		if username == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		sendRequestParseBody(fmt.Sprintf("providers-rust:8084/github?username=%s", username))
+	}
+
 }
