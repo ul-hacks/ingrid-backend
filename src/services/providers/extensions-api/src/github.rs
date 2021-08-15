043e5d50a0ad016e375cdce0b591d669187b3c72
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-08-14 20:12:47 -0400

    failed rust app

--- src/services/providers/extensions-api/src/github.rs
+++ src/services/providers/extensions-api/src/github.rs
@@ -1,3 +1,10 @@
-pub fn get_info(a: String) -> String{
-    return a;
-}
+use warp::{Filter, http::Request};
+use std::collections::HashMap;
+pub fn get_info(a: String) -> HashMap<&'static str,String>{
+    println!("ehhe");
+    let mut info = HashMap::new();
+    info.insert("Hello","pp".to_string());
+    info.insert("UX Design","hoho".to_string());
+    return info;
+    
+} 
--- src/services/providers/extensions-api/src/main.rs
+++ src/services/providers/extensions-api/src/main.rs
@@ -1,15 +1,68 @@
-use warp::{Filter};
-mod github;
-#[tokio::main]
-async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let github = warp::path!("github" / String)
-        .map(|username| github::get_info(username));
-    // let khanacademy = warp::path!();
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-    warp::serve(github)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+// use warp::{Filter};
+// mod github;
 
+// async fn main() {
+
+//     let github = warp::path!("github" / String)
+//         .map(|username| warp::reply::json(&github::get_info(username)));
+    
+    
+//     warp::serve(github)
+//         .run(([127, 0, 0, 1], 3030))
+//         .await;
+
+// }
+use std::collections::HashMap;
+use reqwest::header::AUTHORIZATION;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let resp = client.post("http://httpbin.org/post")
+    .header(AUTHORIZATION,"bearer0")
+    .body("
+    {
+      user(login: \"$nithinmuthukumar\") {
+        login
+        avatarUrl
+        followers {
+          totalCount
+        }
+        following {
+          totalCount
+        }
+        repositories {
+          totalCount
+        }
+        contributionsCollection {
+          contributionCalendar {
+            totalContributions
+          }
+        }
+        itemShowcase {
+          items(first: 6) {
+            edges {
+              node {
+                ... on Repository {
+                  nameWithOwner
+                  description
+                  primaryLanguage {
+                    name
+                    color
+                  }
+                  forkCount
+                  stargazerCount
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    } 
+  ")
+    .send()
+    .await?;
+    println!("{:#?}", resp);
+    println!("{}",AUTHORIZATION);
+    Ok(())
 }
